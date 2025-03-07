081eef756395af01dd8c4d91f00163cf482658a9
santanna37 <santanna.ilha@gmail.com>
2025-03-07 19:31:23 -0300

    c4 - FEAT: Contrução dos teste de use cases e interfaces

--- src/data/use_case/user_finder.py
+++ src/data/use_case/user_finder.py
@@ -5,8 +5,25 @@ from typing import Dict
 
 class UserFinder(UserFinderInterface):
     
-    def __init__(self,user_repository: UsersRepositoryInterface) -> None:
-        self.__user_repository = user_repository
+    def __init__(self,users_repository: UsersRepositoryInterface) -> None:
+        self.__users_repository = users_repository
 
     def find(self, first_name: str) -> Dict:
-        pass 
+        
+        if not first_name.isalpha():
+            raise Exception("Nome invalido")
+        
+        if len(first_name) > 18:
+            raise Exception("Nome grande")
+
+        users == self.__users_repository.select_user(first_name)
+        if users == []:
+            raise Exception("Nome não encontrado")
+
+        response ={
+            "type": "Users",
+            "count":len(users),
+            "atributes": users  
+        }
+
+        retur response 
